2437a4ca3182663b39eae47b70e5b256604db98a
kuankuan <2163826131@qq.com>
2026-05-06 15:58:47 +0800

    fix: add timing for KMarkdownParser initialization in tests

--- test/src/index.ts
+++ test/src/index.ts
@@ -2,5 +2,7 @@ setInterval(() => {}, 1000);
 import fs from 'node:fs';
 import { KMarkdownParser } from '../../src/index.js';
 const testCode = fs.readFileSync('./test/test.md', 'utf-8');
+console.time('parse');
 const a = new KMarkdownParser({ autoParseLink: false });
+console.timeEnd('parse');
 console.log(a.parse(testCode));
